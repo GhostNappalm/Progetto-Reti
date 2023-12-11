ecbe08c81199f0048951224aa7a17cda78bdc32d
MattiaVenuto <m.venuto1@studenti.uniba.it>
2023-12-11 16:13:14 +0100

    committ

--- Proj/obj/Debug/net8.0/Proj.AssemblyInfo.cs
+++ Proj/obj/Debug/net8.0/Proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424be680b1983d0503271c8b44a9cf16457db608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
